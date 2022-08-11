491c840891557b147ae3ba6e4411b4ee5fc346dc
MartinIchu46 <martin.i.ichu@gmail.com>
2022-08-10 22:34:19 -0300

    entrega 4 algunos cambios

--- entrega4/modulos/productos.js
+++ entrega4/modulos/productos.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const { Router } = express;
 
-const router = Router();
+class Productos {
+    constructor(products=[]) {
+         this.products = products;
+    }
+    save(obj) {
+        if(this.products.length>=1){
+            const ids = this.products.map(object => {
+                 return object.id;
+            });
+            
+            const max = Math.max(ids);
+            obj.id= max+1 
+            
+            this.products.push(obj)
+        } else{
+            obj.id=1
+            this.products.push(obj) 
+        }
+    }
+    getById(id) {
+        let product = this.products.find(p => p.id===id) 
+        return product 
+    }
+    deleteById(id) {
+        let index = this.products.findIndex((p) =>p.id===id) 
+        this.products.splice(index,1) 
+    }
+    updateById(id,obj) {
+        let index = this.products.findIndex((p) =>p.id===id)
+        obj[id] = parseInt(id)
+        this.products.splice(index,1) 
+        this.products.push(obj)
+        
+        return obj 
+        }
 
-const productos = [
-    {
-        title: '',
-        price: 2,
-        thumbnauk: '',
-    },
-    
-]
+}
 
 router.get('/', (req, res) => {
     res.send(productos)
